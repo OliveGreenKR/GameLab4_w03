6427b4f45b46a5cc133746ce15ed61aa3ff1e1ce
GameLab4th_leejungmin <lkswasd@gmail.com>
2025-09-03 17:01:33 +0900

    이동 코드 글로벌 추가 및 에러 제거

--- Assets/Scripts/obstacleMove.cs
+++ Assets/Scripts/obstacleMove.cs
@@ -3,66 +3,71 @@ using UnityEngine;
 [System.Serializable]
 public class posData//이동할 방향과 시간 저장
 {
-    public float moveSec=5f;
+    public bool moveGlobal;
+    public float moveSec;
     public float moveSpeed;
     public Vector3 moveDir;
     public Vector3 rotateDir;
 }
 public class obstacleMove : MonoBehaviour
 {
-    public bool comeBack;//이동후 되돌아옴 여부
-    public int moveMax;//배열길이
+    public bool repeat = true;//이동후 되돌아옴 여부
+    private int moveMax;//배열길이
     private int frontAndBack = 1;// 1=front -1=back 
-    private int movingNum=0;//이동중인 posData
-    private float movingSec;//이동중인 시간
+    public int movingNum=0;//이동중인 posData
+    public float movingSec;//이동중인 시간
+    private int test=1;
     public List<posData> movePos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //첫번째 값으로 세팅
-        moveMax = movePos.Count;
-        movingSec = movePos[0].moveSec;
+        if (movePos.Count > 0)
+        {
+            moveMax = movePos.Count;
+            Debug.Log(moveMax);
+            movingSec = movePos[movingNum].moveSec;//이동중시간을 입력한 첫번째 시간으로
+        }
+            
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (moveMax > 0)
+        
+        if (movingSec <= 0)//이동시간이 다되었을 때
         {
-            if (movingSec <= 0)//이동시간이 다되면
+
+            if (movingNum + frontAndBack > moveMax-1 || movingNum+frontAndBack < 0)//다음 이동순서가 없다면
             {
-                if (frontAndBack == 1)
+                if (repeat)//반복 활성화시
                 {
-                    movingNum++;
+                    frontAndBack *= -1;//이동 방향 전환
                 }
-                else if (frontAndBack == -1)
+                else
                 {
-                    movingNum--;
+                    movingNum = 0;//초기 이동으로
                 }
-
-                if (movingNum > moveMax || movingNum < 0)//이동순서가 범위를 넘어가면
-                {
-                    if (comeBack)
-                    {
-                        frontAndBack *= -1;
-                    }
-                    else
-                    {
-                        movingNum = 0;
-                    }
-                }
-                movingSec = movePos[movingNum].moveSec;//다음 이동 시간 적용
             }
-            else//이동시간 남으면
+            else//정상 이동중이면
             {
-                movingSec -= Time.deltaTime;//이동시간 감소
+                movingNum += frontAndBack; //방향에 따라 다음 이동+-1
             }
-
-
-
-            transform.Translate(movePos[movingNum].moveDir * movePos[movingNum].moveSpeed * Time.deltaTime * frontAndBack);
-            transform.Rotate(movePos[movingNum].rotateDir * Time.deltaTime * frontAndBack);
-
+            movingSec = movePos[movingNum].moveSec;//다음 이동 시간 적용
         }
+        else//이동시간 남으면
+        {
+            movingSec -= Time.deltaTime;//이동시간 감소
+        }
+        if (movePos[movingNum].moveGlobal)
+        {
+            transform.Translate(movePos[movingNum].moveDir * movePos[movingNum].moveSpeed * Time.deltaTime * frontAndBack,Space.World);
+        }
+        else
+        {
+            transform.Translate(movePos[movingNum].moveDir * movePos[movingNum].moveSpeed * Time.deltaTime * frontAndBack);
+        }        
+        transform.Rotate(movePos[movingNum].rotateDir * Time.deltaTime * frontAndBack);
     }
+    
 }
