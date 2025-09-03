5645373ec890538c4c6a8e6841515ca0b66c75cb
GameLab4th_leejungmin <lkswasd@gmail.com>
2025-09-03 10:17:35 +0900

    try pull

--- Assets/Scripts/obstacleMove.cs
+++ Assets/Scripts/obstacleMove.cs
@@ -1,20 +1,40 @@
+using System.Collections.Generic;
 using UnityEngine;
-
-public class obstacleMove : MonoBehaviour
+[System.Serializable]
+public class posData//이동할 방향과 시간 저장
 {
+    public string playerName;
+    public float moveSec;
     public float moveSpeed;
     public Vector3 moveDir;
-    public Vector3 rotateDir;
+    public Vector3 rotateDir;    
+}
+public class obstacleMove : MonoBehaviour
+{
+    public bool comeBack;//이동후 되돌아옴 여부
+    public int moveNum;//배열길이
+    private int movingNum;//이동중인 posData
+    private float movingSec;//이동중인 시간
+    public List<posData> movePos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        moveNum = movePos.Count;
+        movingSec = movePos[0].moveSec;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(moveDir * moveSpeed*Time.deltaTime);
-        transform.Rotate(rotateDir* Time.deltaTime);
+        if (movingSec<=0)
+        {
+
+            movingNum++;
+        }
+        
+        transform.Translate(movePos[movingNum].moveDir * movePos[movingNum].moveSpeed * Time.deltaTime);
+        transform.Rotate(movePos[movingNum].rotateDir * Time.deltaTime);
+        movePos[movingNum].moveSec -=Time.deltaTime ;
+
     }
 }
