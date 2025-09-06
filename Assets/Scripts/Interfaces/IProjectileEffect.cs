a580488f654a58f7fc79d66148ae766df4ac3009
OliveGreenKR <olivegreenkr98@gmail.com>
2025-09-06 19:15:50 +0900

    ProjectileBase

---
+++ Assets/Scripts/Interfaces/IProjectileEffect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public interface IProjectileEffect
+{
+    /// <summary>
+    /// 효과 실행 우선순위 (낮을수록 먼저 실행)
+    /// </summary>
+    int Priority { get; }
+
+    /// <summary>
+    /// 투사체에 효과가 추가될 때 호출됩니다
+    /// </summary>
+    /// <param name="owner">소유 투사체</param>
+    /// <param name="existingEffects">기존 효과 목록</param>
+    /// <returns>추가 성공 여부 (false시 기존 효과 수정으로 처리)</returns>
+    bool OnAttachedProjectile(ProjectileBase owner, List<IProjectileEffect> existingEffects);
+
+    /// <summary>
+    /// 투사체가 대상과 충돌했을 때 호출됩니다
+    /// </summary>
+    /// <param name="owner">소유 투사체</param>
+    /// <param name="target">충돌 대상</param>
+    void OnHit(ProjectileBase owner, Collider target);
+
+    /// <summary>
+    /// 투사체가 생명시간 종료로 소멸할 때 호출됩니다
+    /// </summary>
+    /// <param name="owner">소유 투사체</param>
+    void OnDestroy(ProjectileBase owner);
+
+    /// <summary>
+    /// 매 프레임 호출됩니다 (추적 등 지속 효과용)
+    /// </summary>
+    /// <param name="owner">소유 투사체</param>
+    void OnUpdate(ProjectileBase owner);
+}
--- Assets/Scripts/Projectile/ProjectileBase.cs
+++ Assets/Scripts/Projectile/ProjectileBase.cs
@@ -1,10 +1,190 @@
 ﻿using Sirenix.OdinInspector;
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class ProjectileBase : MonoBehaviour
+/// <summary>
+///  Projectile Base, 이펙트 부착이 가능하며
+/// 생명 주기는 스스로 관리. 매니저를 통해 생명주기의 완료 프로세스를 위탁
+/// </summary>
+public abstract class ProjectileBase : MonoBehaviour
 {
-    // ProjectileBase implementation
-}
+    #region Serialized Fields
+    [TabGroup("Movement")]
+    [Header("Movement Settings")]
+    [SuffixLabel("units/sec")]
+    [SerializeField] private float _forwardSpeedUnitsPerSecond = 10f;
 
+    [TabGroup("Lifetime")]
+    [Header("Lifetime Settings")]
+    [SuffixLabel("seconds")]
+    [SerializeField] private float _lifetimeSeconds = 5f;
+
+    [TabGroup("Components")]
+    [Header("Attack Trigger")]
+    [Required]
+    [SerializeField] protected Collider _attackTrigger;
+    #endregion
+
+    #region Properties
+    [TabGroup("Debug")]
+    [ShowInInspector, ReadOnly]
+    public Collider AttackTrigger => _attackTrigger;
+
+    [TabGroup("Debug")]
+    [ShowInInspector, ReadOnly]
+    public float ForwardSpeed => _forwardSpeedUnitsPerSecond;
+
+    [TabGroup("Debug")]
+    [ShowInInspector, ReadOnly]
+    public float RemainingLifetime => _remainingLifetime;
+
+    [TabGroup("Debug")]
+    [ShowInInspector, ReadOnly]
+    public bool IsActive => gameObject.activeInHierarchy;
+    #endregion
+
+    #region Private Fields
+    private float _remainingLifetime;
+
+    [SerializeField]
+    private List<IProjectileEffect> _effects = new List<IProjectileEffect>();
+    #endregion
+
+    #region Unity Lifecycle
+    private void Update()
+    {
+        UpdateLifetime();
+        GoForward();
+        ProcessEffectsOnUpdate();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ProcessEffectsOnHit(other);
+    }
+
+    private void OnEnable()
+    {
+        _remainingLifetime = _lifetimeSeconds;
+        OnProjectileActivated();
+    }
+
+    private void OnDisable()
+    {
+        OnProjectileDeactivated();
+    }
+    #endregion
+
+    #region Public Methods
+    public void Initialize(float lifetimeSeconds = -1f, float forwardSpeed = -1f)
+    {
+        if (lifetimeSeconds > 0f)
+        {
+            _lifetimeSeconds = lifetimeSeconds;
+        }
+
+        if (forwardSpeed > 0f)
+        {
+            _forwardSpeedUnitsPerSecond = forwardSpeed;
+        }
+
+        _remainingLifetime = _lifetimeSeconds;
+    }
+
+    public void AddEffect(IProjectileEffect effect)
+    {
+        if (effect == null) return;
+
+        bool shouldAdd = effect.OnAttachedProjectile(this, _effects);
+
+        if (shouldAdd)
+        {
+            _effects.Add(effect);
+            SortEffectsByPriority();
+        }
+    }
+
+    public void RemoveEffect(IProjectileEffect effect)
+    {
+        if (effect != null)
+        {
+            _effects.Remove(effect);
+        }
+    }
+
+    public void DestroyProjectile()
+    {
+        ProcessEffectsOnDestroy();
+
+        if (ProjectileManager.Instance != null)
+        {
+            ProjectileManager.Instance.ReturnProjectile(this);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    #endregion
+
+    #region Protected Virtual Methods
+    protected virtual void GoForward()
+    {
+        transform.Translate(Vector3.forward * _forwardSpeedUnitsPerSecond * Time.deltaTime);
+    }
+
+    protected virtual void OnProjectileActivated()
+    {
+        // 자식 클래스에서 오버라이드
+    }
+
+    protected virtual void OnProjectileDeactivated()
+    {
+        // 자식 클래스에서 오버라이드
+    }
+    #endregion
+
+    #region Private Methods
+    private void UpdateLifetime()
+    {
+        _remainingLifetime -= Time.deltaTime;
+
+        if (_remainingLifetime <= 0f)
+        {
+            DestroyProjectile();
+        }
+    }
+
+    private void ProcessEffectsOnHit(Collider target)
+    {
+        for (int i = 0; i < _effects.Count; i++)
+        {
+            _effects[i].OnHit(this, target);
+        }
+    }
+
+    private void ProcessEffectsOnDestroy()
+    {
+        for (int i = 0; i < _effects.Count; i++)
+        {
+            _effects[i].OnDestroy(this);
+        }
+
+        _effects.Clear();
+    }
+
+    private void ProcessEffectsOnUpdate()
+    {
+        for (int i = 0; i < _effects.Count; i++)
+        {
+            _effects[i].OnUpdate(this);
+        }
+    }
+
+    private void SortEffectsByPriority()
+    {
+        _effects = _effects.OrderBy(effect => effect.Priority).ToList();
+    }
+    #endregion
+}
