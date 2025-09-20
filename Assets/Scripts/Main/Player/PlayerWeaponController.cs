62ce9e2b18c812aef1fb783d78ec56bb618256d6
OliveGreenKR <olivegreenkr98@gmail.com>
2025-09-20 16:05:03 +0900

    before refact WeaponSYstem

---
+++ Assets/Scripts/Main/Player/PlayerWeaponController.cs
@@ -0,0 +1,93 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class PlayerWeaponController : MonoBehaviour
+{
+    #region Serialized Fields
+    [TabGroup("References")]
+    [Header("Core References")]
+    [Required]
+    [SerializeField] private ProjectileLauncher _projectileLauncher;
+
+    [TabGroup("References")]
+    [Required]
+    [SerializeField] private AccuracySystem _accuracySystem;
+
+    [TabGroup("Weapon")]
+    [Header("Weapon Configuration")]
+    [Required]
+    [SerializeField] private WeaponStatSO _weaponStatSO;
+
+    [TabGroup("Settings")]
+    [Header("Input Settings")]
+    [SerializeField] private bool _enableInput = true;
+
+    [TabGroup("Settings")]
+    [Header("Mode Switch Settings")]
+    [SuffixLabel("seconds")]
+    [PropertyRange(0.1f, 2f)]
+    [SerializeField] private float _modeSwitchCooldown = 0.5f;
+    #endregion
+
+    #region Properties
+    [TabGroup("Debug")]
+    [ShowInInspector, ReadOnly]
+    public WeaponStatData CurrentWeaponStats { get; private set; }
+
+    [TabGroup("Debug")]
+    [ShowInInspector, ReadOnly]
+    public WeaponMode CurrentMode { get; private set; }
+
+    [TabGroup("Debug")]
+    [ShowInInspector, ReadOnly]
+    public bool CanFire { get; private set; }
+
+    [TabGroup("Debug")]
+    [ShowInInspector, ReadOnly]
+    public float FireCooldownRemaining { get; private set; }
+
+    [TabGroup("Debug")]
+    [ShowInInspector, ReadOnly]
+    public float ModeSwitchCooldownRemaining { get; private set; }
+    #endregion
+
+    #region Private Fields
+    private bool _isFiring = false;
+    private float _nextFireTime = 0f;
+    private float _nextModeSwitchTime = 0f;
+    #endregion
+
+    #region Unity Lifecycle
+    private void Awake() { }
+    private void Start() { }
+    private void Update() { }
+    #endregion
+
+    #region Public Methods - Weapon Control
+    public void StartFiring() { }
+    public void StopFiring() { }
+    public bool TryFire() { }
+    public void SwitchToMode(WeaponMode mode) { }
+    public void CycleModeNext() { }
+    #endregion
+
+    #region Public Methods - Stat Management
+    public void UpgradeFireRate(float amount) { }
+    public void UpgradeDamage(float amount) { }
+    public void UpgradeAccuracy(float amount) { }
+    public void SetWeaponStatSO(WeaponStatSO weaponStatSO) { }
+    #endregion
+
+    #region Private Methods - Fire Logic
+    private bool CanFireNow() { }
+    private void ExecuteFire() { }
+    private Vector3 CalculateFireDirection() { }
+    private void ApplyFireCooldown() { }
+    #endregion
+
+    #region Private Methods - Mode Management
+    private void ApplyModeToStats() { }
+    private void UpdateCooldowns() { }
+    private void InitializeWeaponStats() { }
+    #endregion
+}
